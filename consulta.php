c3a8e6f88f5d70e092d1ef9490d3d26fadd3e9f8
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-08-07 23:16:57 -0300

    feat: site name was changed

--- consulta.php
+++ consulta.php
@@ -16,7 +16,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_web_banco_de_dados/controller
 <body class="bg-dark">
     <nav class="navbar navbar-expand-lg bg-black mb-5">
         <div class="container">
-            <a class="navbar-brand text-light" href="index.php">Form MJS</a>
+            <a class="navbar-brand text-light" href="index.php">Form MathSant</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <body class="bg-dark">
     <nav class="navbar navbar-expand-lg bg-black mb-5">
         <div class="container">
-            <a class="navbar-brand text-light" href="#">Form MJS</a>
+            <a class="navbar-brand text-light" href="#">Form MathSant</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
